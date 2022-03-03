d5eea640ddeb04cf3696cb50b1296591cf6c7779
Simon Cropp <simon.cropp@gmail.com>
2022-03-03 14:33:09 +1100

    Update SimpleTypeTests.cs

--- src/Verify.Tests/SimpleTypeTests.cs
+++ src/Verify.Tests/SimpleTypeTests.cs
@@ -4,7 +4,7 @@ using System.Xml.Linq;
 [UsesVerify]
 public class SimpleTypeTests
 {
-    #if NET5_0_OR_GREATER
+#if NET5_0_OR_GREATER
     [Theory]
     [MemberData(nameof(GetData))]
     public Task Run(object arg)
@@ -13,7 +13,7 @@ public class SimpleTypeTests
         settings.UseParameters(arg.GetType());
         return Verify(arg, settings);
     }
-    #endif
+#endif
 
     [Fact]
     public Task StringWrappedInTask()
@@ -46,7 +46,6 @@ public class SimpleTypeTests
     }
 
 #if NET5_0_OR_GREATER
-
     [Fact]
     public Task DateTimeWrappedInTask()
     {
@@ -67,13 +66,13 @@ public class SimpleTypeTests
         var xmlDocument = new XmlDocument();
         xmlDocument.LoadXml(xml);
 
-        yield return new object[] {xmlDocument};
+        yield return new object[] { xmlDocument };
 
         var xDocument = XDocument.Parse(xml);
 
-        yield return new object[] {xDocument};
+        yield return new object[] { xDocument };
 
-        yield return new object[] {xDocument.Root!};
+        yield return new object[] { xDocument.Root! };
 
         var json = @"{
   'short': {
@@ -87,25 +86,25 @@ public class SimpleTypeTests
 }";
         var jToken = JToken.Parse(json);
 
-        yield return new object[] {jToken};
+        yield return new object[] { jToken };
 
-        yield return new object[] {"theString"};
-        yield return new object[] {true};
-        yield return new object[] {(long) 1};
-        yield return new object[] {(short) 1};
-        yield return new object[] {1};
-        yield return new object[] {(uint) 1};
-        yield return new object[] {(ulong) 1};
-        yield return new object[] {(ushort) 1};
-        yield return new object[] {(decimal) 1.1};
-        yield return new object[] {(float) 1.1};
-        yield return new object[] {new Guid("ebced679-45d3-4653-8791-3d969c4a986c")};
-        yield return new object[] {new DateTime(2000, 1, 1, 1, 1, 1, DateTimeKind.Utc).ToUniversalTime()};
-        yield return new object[] {new DateTimeOffset(2000, 1, 1, 1, 1, 1, 1, TimeSpan.FromHours(1)).ToUniversalTime()};
-        #if NET6_0_OR_GREATER
+        yield return new object[] { "theString" };
+        yield return new object[] { true };
+        yield return new object[] { (long)1 };
+        yield return new object[] { (short)1 };
+        yield return new object[] { 1 };
+        yield return new object[] { (uint)1 };
+        yield return new object[] { (ulong)1 };
+        yield return new object[] { (ushort)1 };
+        yield return new object[] { (decimal)1.1 };
+        yield return new object[] { (float)1.1 };
+        yield return new object[] { new Guid("ebced679-45d3-4653-8791-3d969c4a986c") };
+        yield return new object[] { new DateTime(2000, 1, 1, 1, 1, 1, DateTimeKind.Utc).ToUniversalTime() };
+        yield return new object[] { new DateTimeOffset(2000, 1, 1, 1, 1, 1, 1, TimeSpan.FromHours(1)).ToUniversalTime() };
+#if NET6_0_OR_GREATER
         yield return new object[] {(Half)10};
         yield return new object[] {new DateOnly(2000, 1, 1)};
         yield return new object[] {new TimeOnly(1, 1)};
-        #endif
+#endif
     }
 }
